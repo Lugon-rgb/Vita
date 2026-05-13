861d85feb8a3952815b8a18a4e6dcdc84c0e195d
Lugon <149012228+Lugon-rgb@users.noreply.github.com>
2026-05-13 14:01:41 -0300

    organização geral

---
+++ lib/pages/quiz_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class TelaQuiz extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Quiz")),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Text("Você treinou hoje?", style: TextStyle(fontSize: 22)),
+
+            SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context, {'vida': 10, 'estamina': 5});
+              },
+              child: Text("Sim"),
+            ),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context, {'vida': -5, 'estamina': -5});
+              },
+              child: Text("Não"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
