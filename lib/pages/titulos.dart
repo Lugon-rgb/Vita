a8773462681621b7cb1a12d6864bef2ce6ec351e
Juanzin26 <juanzitcho07@gmail.com>
2026-05-19 16:14:50 +0100

    mudança no visual dos titulos e conquistas

--- lib/pages/titulos.dart
+++ lib/pages/titulos.dart
@@ -28,90 +28,90 @@ static const Color misterioso = Color.fromARGB(255, 218, 32, 32);
 
           // TÍTULOS COMUNS:
           titulo('Novato', 
-          'Título default, concedido ao ingressar no Vita.', Icons.lock_open, comum),
+          'Título default, concedido ao ingressar no Vita.', Icons.check_circle, comum),
           titulo('Novato Determinado', 
-          'Atingir a conquista “Primeira Vitória”.', Icons.lock_open, comum),
+          'Atingir a conquista “Primeira Vitória”.', Icons.check_circle, comum),
           titulo('Escrivão', 
-          'Atingir a conquista “Anotação de Campo”.', Icons.lock, comum),
+          'Atingir a conquista “Anotação de Campo”.', Icons.lock_outline, comum),
           titulo('Vigia das Moedas', 
-          'Atingir as conquistas “Pilar de Finanças” e “Registro de Ouro”.', Icons.lock_open, comum),
+          'Atingir as conquistas “Pilar de Finanças” e “Registro de Ouro”.', Icons.check_circle, comum),
           titulo('Andarilho', 
-          'Atingir o nível 5.', Icons.lock_open, comum),
+          'Atingir o nível 5.', Icons.check_circle, comum),
 
 
           rankTitulos('Títulos Incomuns', incomum),
 
           // TÍTULOS INCOMUNS:
           titulo('Guerreiro do Foco', 
-          'Atingir a conquista “Ritmo Semanal”.', Icons.lock_open, incomum),
+          'Atingir a conquista “Ritmo Semanal”.', Icons.check_circle, incomum),
           titulo('Sábio Resistente', 
-          'Atingir as conquistas “Sede de Conhecimento” e “Elixir da Vida”.', Icons.lock_open, incomum),
+          'Atingir as conquistas “Sede de Conhecimento” e “Elixir da Vida”.', Icons.check_circle, incomum),
           titulo('Guardião das Memórias', 
-          'Atingir a conquista “Mosaico de Ideias”.', Icons.lock, incomum),
+          'Atingir a conquista “Mosaico de Ideias”.', Icons.lock_outline, incomum),
           titulo('Guardião das Moedas', 
-          'Atingir a conquista “Disciplina Financeira”.', Icons.lock, incomum),
+          'Atingir a conquista “Disciplina Financeira”.', Icons.lock_outline, incomum),
           titulo('Veterano de Batalhas', 
-          'Atingir o nível 15.', Icons.lock_open, incomum),
+          'Atingir o nível 15.', Icons.check_circle, incomum),
 
 
           rankTitulos('Títulos Raros', raro),
 
           // TÍTULOS RAROS:
           titulo('Invulnerável', 
-          'Atingir as conquistas “Armadura de Ouro” e “Vigor Pleno”.', Icons.lock, raro),
+          'Atingir as conquistas “Armadura de Ouro” e “Vigor Pleno”.', Icons.lock_outline, raro),
           titulo('Mestre do Tesouro', 
-          'Atingir as conquistas “Superávit” e “Muralha financeira”.', Icons.lock_open, raro),
+          'Atingir as conquistas “Superávit” e “Muralha financeira”.', Icons.check_circle, raro),
           titulo('Sentinela do Tempo', 
-          'Atingir a conquista “Sequência de Guerreiro”.', Icons.lock, raro),
+          'Atingir a conquista “Sequência de Guerreiro”.', Icons.lock_outline, raro),
           titulo('Guardião Experiente', 
-          'Atingir o nível 30.', Icons.lock_open, raro),
+          'Atingir o nível 30.', Icons.check_circle, raro),
       
 
           rankTitulos('Títulos Épicos', epico),
 
           // TÍTULOS ÉPICOS:
           titulo('Imaculado', 
-          'Atingir as conquistas “Fortaleza Pessoal” e “Jornada da Perfeição”.', Icons.lock, epico),
+          'Atingir as conquistas “Fortaleza Pessoal” e “Jornada da Perfeição”.', Icons.lock_outline, epico),
           titulo('Governador do Destino', 
-          'Atingir as conquistas “Caminho Estruturado” e “Mapa Completo”', Icons.lock_open, epico),
+          'Atingir as conquistas “Caminho Estruturado” e “Mapa Completo”', Icons.check_circle, epico),
           titulo('Clarividente', 
-          'Atingir a conquista “Mente de Cristal”.', Icons.lock, epico),
+          'Atingir a conquista “Mente de Cristal”.', Icons.lock_outline, epico),
           titulo('Magnata', 
-          'Atingir a conquista “Sonho Realizado”.', Icons.lock_open, epico),
+          'Atingir a conquista “Sonho Realizado”.', Icons.check_circle, epico),
           titulo('Campeão Implacável', 
-          'Atingir o nível 50.', Icons.lock_open, epico),
+          'Atingir o nível 50.', Icons.check_circle, epico),
 
 
           rankTitulos('Títulos Lendários', lendario),
 
           // TÍTULOS LENDÁRIOS:
           titulo('Espartano', 
-          'Atingir a conquista “Eternidade Vita”.', Icons.lock, lendario),
+          'Atingir a conquista “Eternidade Vita”.', Icons.lock_outline, lendario),
           titulo('Uzumaki', 
-          'Atingir a conquista “Ciclo Perfeito”.', Icons.lock, lendario),
+          'Atingir a conquista “Ciclo Perfeito”.', Icons.lock_outline, lendario),
           titulo('Demiurgo', 
-          'Atingir as conquistas “Plano de Uma Era” e “Atlas Universal”.', Icons.lock_open, lendario),
+          'Atingir as conquistas “Plano de Uma Era” e “Atlas Universal”.', Icons.check_circle, lendario),
           titulo('Midas', 
-          'Atingir as conquistas “Cofre de ferro” e “Atlas Universal”.', Icons.lock, lendario),
+          'Atingir as conquistas “Cofre de ferro” e “Atlas Universal”.', Icons.lock_outline, lendario),
           titulo('O Imortalizado', 
-          'Atingir o nível 75.', Icons.lock_open, lendario),
+          'Atingir o nível 75.', Icons.check_circle, lendario),
 
 
           rankTitulos('???',misterioso),
 
           // TÍTULOS OCULTOS: 
           titulo('O Inominável', 
-          'Atinge ao salvar uma nota sem preencher o título.', Icons.lock_open, misterioso),
+          'Atinge ao salvar uma nota sem preencher o título.', Icons.check_circle, misterioso),
           titulo('Mestre do Centavo', 
-          '???', Icons.lock, misterioso),
+          '???', Icons.lock_outline, misterioso),
           titulo('Alma Gêmea', 
-          '???', Icons.lock, misterioso),
+          '???', Icons.lock_outline, misterioso),
           titulo('Minimalista absoluto', 
-          'Atinge ao ter 0 metas ativas, 0 notas, 0 gastos, mas entrar no app por 7 dias seguidos.', Icons.lock_open, misterioso),
+          'Atinge ao ter 0 metas ativas, 0 notas, 0 gastos, mas entrar no app por 7 dias seguidos.', Icons.check_circle, misterioso),
           titulo('Sobrevivente do Caos', 
-          '???', Icons.lock, misterioso),
+          '???', Icons.lock_outline, misterioso),
           titulo('Immortalis Vita', 
-          'Alcançar o nível 100', Icons.lock_open, misterioso),
+          'Alcançar o nível 100', Icons.check_circle, misterioso),
 
         ],
       ),
@@ -138,6 +138,18 @@ static const Color misterioso = Color.fromARGB(255, 218, 32, 32);
 
 // criando função para criar cada conquista + descrição + ícone + cor da raridade p colorir o circulo do icone
   Widget titulo (String nome, String descricao, IconData icone, Color corRaridade) {
+
+final bool ehDesbloqueado = icone == Icons.check_circle;
+
+    // desbloqueado : bloqueado
+    final Color corTextoTitulo = ehDesbloqueado ? Colors.white : const Color.fromRGBO(255, 255, 255, 0.384);
+    final Color corTextoDescricao = ehDesbloqueado ? const Color.fromRGBO(255, 255, 255, 0.702) : const Color.fromRGBO(255, 255, 255, 0.239);
+    
+    final Color corIcone = ehDesbloqueado ? corRaridade : const Color.fromRGBO(117, 117, 117, 1);
+    
+    final Color corBordaCirculo = ehDesbloqueado ? corRaridade.withValues(alpha: 0.4) : const Color.fromRGBO(255, 255, 255, 0.102);
+    final Color corFundoCirculo = ehDesbloqueado ? corRaridade.withValues(alpha: 0.1) : Colors.transparent;
+
     return Card(
       color: const Color(0xFF1E1E1E),
       margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
@@ -145,14 +157,17 @@ static const Color misterioso = Color.fromARGB(255, 218, 32, 32);
         leading: Container( // container envolta do icone pra colorir com a cor da raridade
           padding: const EdgeInsets.all(8),
           decoration: BoxDecoration(
-            color: corRaridade.withValues(alpha: 0.1), 
+            color: corFundoCirculo,
             shape: BoxShape.circle, // transformei o container num circulo p ficar mais bonitinho
-            border: Border.all(color: corRaridade.withValues(alpha: 0.5), width: 1), 
+            border: Border.all(color: corBordaCirculo, width: 1), 
           ),
-          child: Icon(icone, color: Colors.white ),
+          child: Opacity(
+            opacity: ehDesbloqueado ? 1.0 : 0.6,
+            child: Icon(icone, color: corIcone),
+          ),
         ),
-        title: Text(nome, style: const TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold)),
-        subtitle: Text(descricao, style: const TextStyle(color: Color.fromRGBO(255, 255, 255, 0.702), fontSize: 14)),
+        title: Text(nome, style: TextStyle(color: corTextoTitulo, fontSize: 16, fontWeight: FontWeight.bold)),
+        subtitle: Text(descricao, style: TextStyle(color: corTextoDescricao, fontSize: 14)),
       ),
     );
   }
